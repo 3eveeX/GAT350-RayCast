0c3e5775f4ee3c09fd47e9773cbc5c2bc311f565
3eveeX <hatchjavan@gmail.com>
2025-12-02 10:19:50 -0700

    added the camera

---
+++ RayTracer/Source/Camera.cpp
@@ -0,0 +1,37 @@
+#include "Camera.h"
+
+void Camera::SetView(const glm::vec3& eye, const glm::vec3& target, const glm::vec3& up) {
+	this->eye = eye;
+
+	// create camera axis
+	this->forward = glm::normalize(target - eye);
+	this->right = glm::normalize(glm::cross(forward, up));
+	this->up = glm::normalize(glm::cross(right, forward));
+
+	CalculateViewPlane();
+}
+
+Ray Camera::GetRay(const glm::vec2& uv) const {
+	Ray ray;
+
+	 
+	ray.origin = eye;	 
+	ray.direction = lowerLeft + horizontal * uv.x + vertical * uv.y - eye;
+
+	return ray;
+}
+
+void Camera::CalculateViewPlane() {
+	
+	float theta = glm::radians(fov);
+	
+	float halfHeight = glm::tan(theta / 2);
+	
+	float halfWidth = halfHeight * aspectRatio;
+
+	
+	horizontal = right * halfWidth * 2.0f;
+	vertical = up * halfHeight * 2.0f;
+	lowerLeft = eye - halfWidth - halfHeight + forward;
+}
+
